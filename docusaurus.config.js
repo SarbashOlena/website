fc3c5826ad6e3aff4f27777bfc1ed334f5ad9542
SarbashOlena <sarbash.olena@lll.kpi.ua>
2024-05-14 22:41:08 +0300

    Завдання виконано.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
+  // useful metadata like html       lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
